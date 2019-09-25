579ac190946679cf1aac6ff15bda8f2b2d86e0eb
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-25 15:08:56 +0200

    Run test: String_Initialization() and test: Adding_String()

--- CSharp_Basics/CSharp_Basics/StringsAndItsOperations.cs
+++ CSharp_Basics/CSharp_Basics/StringsAndItsOperations.cs
@@ -33,26 +33,30 @@ namespace CSharp_Basics
         //        ///  use string.Empty;
         //        ///  create new strings itsEmpty and make it empty
         //        /// </summary>
-        //        [Test]
-        //        public void String_Initialization()
-        //        {
-
-        //            Assert.That(itsEmpty, Is.Not.Null);
-        //        }
+        [Test]
+        public void String_Initialization()
+        {
+            string xxx = string.Empty;
+            string yyy = "";
+            
+            Assert.That(xxx, Is.Not.Null, "Are you sure it is not null?");
+        }
 
         //        /// <summary>
         //        /// + is not best way of adding string
         //        ///  use string.Concat();
         //        ///  put sentence together
         //        /// </summary>
-        //        [Test]
-        //        public void Adding_String()
-        //        {
-        //            string first = "pancakes.";
-        //            string second = "I like";
-        //
-        //            Assert.AreEqual(result, "I like pancakes.");
-        //        }
+        [Test]
+        public void Adding_String()
+        {
+            string first = "pancakes.";
+            string second = "I like";
+
+            string result = string.Concat(second," ", first);
+
+            Assert.AreEqual(result, "I like pancakes.");
+        }
 
         //        /// <summary>
         //        /// There is few more ways of constructing strings for example if you need add values of other variables inside you can do
