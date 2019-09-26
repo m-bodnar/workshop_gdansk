859f3f36753c3b817c890a14f7fc92263b1db27c
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-26 15:16:08 +0200

    First test: AddingOperation()

--- CSharp_Basics/CalculatorTests/UnitTest1.cs
+++ CSharp_Basics/CalculatorTests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using Calculator;
 
 namespace CalculatorTests
 {
@@ -7,8 +8,12 @@ namespace CalculatorTests
     public class UnitTest1
     {
         [Test]
-        public void TestMethod1()
+        public void AddingOperation()
         {
+            MathData mathData = new MathData(5, 3, "+");
+            var result = Calculations.DoMath(mathData);
+
+            Assert.AreEqual(8, result);
         }
     }
 }
