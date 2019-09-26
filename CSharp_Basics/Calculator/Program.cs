260a2532c0de412eddfa4dfece8995b85db8457d
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-26 12:11:20 +0200

    App is closing when user write q

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -7,122 +7,32 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
+        static bool quit = false;
+
         static void Main(string[] args)
         {
-            //Console.WriteLine("Basic operations on integers:");
-
-            //int x = 4;
-            //int y = 6;
-
-            //int add = x + y;
-            //int subtract = y - x;
-            //int multiply = x * y;
-
-            //Console.WriteLine("x + y =" + " " + add);
-            //Console.WriteLine("y - x =" + " " + subtract);
-            //Console.WriteLine("x * y =" + " " + multiply);
-
-            //// okno pozostaje otwarte; zamyka okno po naciśnięciu Enter
-            //// Console.ReadLine();
-
-            //// okno pozostaje otwarte; zamyka okno po naciśnięciu jakiegokolwiek przycisku
-            //Console.ReadKey();
-
-            //Console.WriteLine("Basic operations on integers:");
-
-            //Console.WriteLine("Podaj wartosc liczby a");
-            //string A = Console.ReadLine();
-            //int parseA = int.Parse(A);
-
-            //Console.WriteLine("Podaj wartosc liczby b");
-            //string b = Console.ReadLine();
-            //int parseB = int.Parse(b);
-
-            //int add = parseA + parseB;
-            //int subtract = parseB - parseA;
-
-            //Console.WriteLine("a + b =" + " " + add);
-            //Console.WriteLine("b - a =" + " " + subtract);
-
-            //// okno pozostaje otwarte; zamyka okno po naciśnięciu Enter
-            //// Console.ReadLine();
-
-            //// okno pozostaje otwarte; zamyka okno po naciśnięciu jakiegokolwiek przycisku
-            //Console.ReadKey();
-
-            //Console.WriteLine("Basic operations on integers:");
-
-            //Console.WriteLine("Podaj wartosc liczby a");
-            //string A = Console.ReadLine();
-            //int parseA = int.Parse(A);
-
-            //Console.WriteLine("Podaj wartosc liczby b");
-            //string b = Console.ReadLine();
-            //int parseB = int.Parse(b);
-
-            //int add = Add(parseA, parseB);
-            //int subtract = Subtract(parseA, parseB);
-            //int multiply = Multiply(parseA, parseB);
-            //int divide = Divide(parseA, parseB);
-
-            //Console.WriteLine("a + b =" + " " + add);
-            //Console.WriteLine("b - a =" + " " + subtract);
-            //Console.WriteLine("a * b =" + " " + multiply);
-            //Console.WriteLine("a / b =" + " " + divide);
-
-            //Console.ReadKey();
-
-            //// + - / *
-            //Console.WriteLine("Basic operations on integers:");
-
-            //Console.WriteLine("Podaj wartosc liczby a");
-            //string A = Console.ReadLine();
-            //int parseA = int.Parse(A);
-
-            //Console.WriteLine("Podaj wartosc liczby b");
-            //string b = Console.ReadLine();
-            //int parseB = int.Parse(b);
-
-            //Console.WriteLine("Podaj operator");
-            //string operation = Console.ReadLine();
-            //double result = 0;
-
-            //if (operation == "-")
-            //{
-            //    result = Subtract(parseA, parseB);
-            //}
-
-            //else if (operation == "+")
-            //{
-            //    result = Add(parseA, parseB);
-            //}
-
-            //else if (operation == "/")
-            //{
-            //    result = Divide(parseA, parseB);
-            //}
-
-            //else if (operation == "*")
-            //{
-            //    result = Multiply(parseA, parseB);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Nieznana operacja");
-            //}
-
-            //Console.WriteLine("Wynik to:" + " " + result);
-
-            //Console.ReadKey();
-
+            
             // bool isRunning = true;
-            while (true)
+            while (!quit)
             {
-
+                
                 int parseA = GetValueFromUser("Podaj wartosc liczby A");
+                if (quit)
+                {
+                    Console.WriteLine("Zamykam aplikację");
+                    Console.ReadKey();
+                    return;
+                }
+
                 int parseB = GetValueFromUser("Podaj wartosc liczby B");
+                if (quit)
+                {
+                    Console.WriteLine("Zamykam aplikację");
+                    Console.ReadKey();
+                    return;
+                }
 
                 double result = 0;
 
@@ -153,6 +63,11 @@ namespace Calculator
             {
                 Console.WriteLine(message);
                 string valueFromUser = Console.ReadLine();
+                if(valueFromUser == "q")
+                {
+                    quit = true;
+                    return 0;
+                }
                 parssed = int.TryParse(valueFromUser, out parsedNumber);
                 if(!parssed)
                 {
