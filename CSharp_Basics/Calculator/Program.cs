0c16ea5d73e8cf8b9b2ee03096eb6a49be3e22ff
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-26 09:49:44 +0200

    New function: User has to add operator (+ - / *)

--- CSharp_Basics/Calculator/Program.cs
+++ CSharp_Basics/Calculator/Program.cs
@@ -51,6 +51,31 @@ namespace Calculator
             //// okno pozostaje otwarte; zamyka okno po naciśnięciu jakiegokolwiek przycisku
             //Console.ReadKey();
 
+            //Console.WriteLine("Basic operations on integers:");
+
+            //Console.WriteLine("Podaj wartosc liczby a");
+            //string a = Console.ReadLine();
+            //int parsea = int.Parse(a);
+
+            //Console.WriteLine("Podaj wartosc liczby b");
+            //string b = Console.ReadLine();
+            //int parseb = int.Parse(b);
+
+            //int add = Add(parsea, parseb);
+            //int subtract = Subtract(parsea, parseb);
+            //int multiply = Multiply(parsea, parseb);
+            //int divide = Divide(parsea, parseb);
+
+            //Console.WriteLine("a + b =" + " " + add);
+            //Console.WriteLine("b - a =" + " " + subtract);
+            //Console.WriteLine("a * b =" + " " + multiply);
+            //Console.WriteLine("a / b =" + " " + divide);
+
+            //Console.ReadKey();
+
+            
+            // + - / *
+
             Console.WriteLine("Basic operations on integers:");
 
             Console.WriteLine("Podaj wartosc liczby a");
@@ -61,15 +86,31 @@ namespace Calculator
             string b = Console.ReadLine();
             int parseb = int.Parse(b);
 
-            int add = Add(parsea, parseb);
-            int subtract = Subtract(parsea, parseb);
-            int multiply = Multiply(parsea, parseb);
-            int divide = Divide(parsea, parseb);
-           
-            Console.WriteLine("a + b =" + " " + add);
-            Console.WriteLine("b - a =" + " " + subtract);
-            Console.WriteLine("a * b =" + " " + multiply);
-            Console.WriteLine("a / b =" + " " + divide);
+            Console.WriteLine("Podaj operator");
+            string operation = Console.ReadLine();
+            double result = 0;
+
+            if (operation == "-")
+            {
+                result = Subtract(parsea, parseb);
+            }
+
+            if (operation == "+")
+            {
+                result = Add(parsea, parseb);
+            }
+
+            if (operation == "/")
+            {
+                result = Divide(parsea, parseb);
+            }
+
+            if (operation == "*")
+            {
+                result = Multiply(parsea, parseb);
+            }
+
+            Console.WriteLine("Wynik to:" + " " + result);
 
             Console.ReadKey();
         }
