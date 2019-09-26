e1f74256925cf311590d187da797d9385d29fe73
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-26 15:34:43 +0200

    Added few Test cases

--- CSharp_Basics/CalculatorTests/UnitTest1.cs
+++ CSharp_Basics/CalculatorTests/UnitTest1.cs
@@ -7,13 +7,59 @@ namespace CalculatorTests
     [TestFixture]
     public class UnitTest1
     {
-        [Test]
-        public void AddingOperation()
+        [TestCase(1,4,5)]
+        [TestCase(-1, 0, -1)]
+        [TestCase(10, 1000, 1010)]
+        [TestCase(-1, -2, -3)]
+        public void AddingOperation_Positive_Values(int firstValue, int secondValue, int expectedResult)
         {
-            MathData mathData = new MathData(5, 3, "+");
+            // Arrange
+            MathData mathData = new MathData(firstValue, secondValue, "+");
+
+            // Act
             var result = Calculations.DoMath(mathData);
 
-            Assert.AreEqual(8, result);
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void AddingOperation_Positive_Negative_Values()
+        {
+            // Arrange
+            MathData mathData = new MathData(1, -1, "+");
+
+            // Act
+            var result = Calculations.DoMath(mathData);
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [Test]
+        public void AddingOperation_Max_Values()
+        {
+            // Arrange
+            MathData mathData = new MathData(int.MaxValue, int.MaxValue, "+");
+
+            // Act
+            var result = Calculations.DoMath(mathData);
+
+            // Assert
+            Assert.AreEqual(int.MaxValue, result);
+        }
+
+        [Test]
+        public void AddingOperation_Big_Values()
+        {
+            // Arrange
+            MathData mathData = new MathData(30000, 31000, "+");
+
+            // Act
+            var result = Calculations.DoMath(mathData);
+
+            // Assert
+            Assert.AreEqual(61000, result);
         }
     }
 }
