f779d5f8806ffa430f8c6f854fbf7f47b573c686
m-bodnar <mbodnar@infoprojekt.pl>
2019-09-27 10:34:35 +0200

    Selenium start

--- CSharp_Basics/SeleniumTest/Class1.cs
+++ CSharp_Basics/SeleniumTest/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace SeleniumTest
@@ -6,10 +7,30 @@ namespace SeleniumTest
     [TestFixture]
     public class Class1
     {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            // dodanie nowej instancji ChromeDriver
+            driver = new ChromeDriver();
+        }
+
         [Test]
         public void FirstTest()
         {
-            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+        }
+
+        // atrybut odpowiadający za 'sprzątanie' - [TearDown]
+        [TearDown]
+        public void TearDown()
+        {
+            // zamykanie przeglądarki (obojętnie czy test się wykona czy nie)
+            // driver.Close() lub driver.Quit()
+            driver.Quit();
         }
 
 
